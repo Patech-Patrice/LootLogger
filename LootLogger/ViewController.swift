72982588fdff619502a42637ffc3382a58aee6a5
Patech_Patrice <patricedrayton86@gmail.com>
2022-02-28 11:28:07 -0500

    Initial Commit

---
+++ LootLogger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LootLogger
+//
+//  Created by Patrice Drayton on 2/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
