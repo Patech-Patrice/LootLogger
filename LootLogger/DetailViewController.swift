b7ccd52f15f1727416039bb5793b9f0384a9c4a7
Patech_Patrice <patricedrayton86@gmail.com>
2022-03-01 12:19:42 -0500

    created detail view controller to show details of items

--- LootLogger/DetailViewController.swift
+++ LootLogger/DetailViewController.swift
@@ -5,4 +5,47 @@
 //  Created by Patrice Drayton on 3/1/22.
 //
 
-import Foundation
+
+
+//to display the information on the selected item create a new subclass for that item
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    //outlet needed in order to access the subview
+    
+    @IBOutlet var nameField: UITextField!
+    @IBOutlet var serialNumberField: UITextField!
+    @IBOutlet var valueField: UITextField!
+    @IBOutlet var dateLabel: UILabel!
+    
+    
+    //add a property for an Item instance and override viewWillAppear(_:) to set up the interface
+    var item: Item!
+    
+    //number formatter
+    let numberFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }()
+    //date formatter
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        nameField.text = item.name
+        serialNumberField.text = item.serialNumber
+        valueField.text = numberFormatter.string(from: NSNumber(value: item.valueInDollars))
+        dateLabel.text = dateFormatter.string(from: item.dateCreated)
+    }
+    
+    
+}
