0db4904a9459b9eb366cff8f67e14596bb86972b
Patech_Patrice <patricedrayton86@gmail.com>
2022-03-04 12:41:37 -0500

    implemented method to save image and display it on the UI

--- LootLogger/DetailViewController.swift
+++ LootLogger/DetailViewController.swift
@@ -12,18 +12,17 @@ import UIKit
 
 //have the DetailViewController conform to the UITextFieldDelegate
 
-class DetailViewController: UIViewController, UITextFieldDelegate {
+class DetailViewController: UIViewController, UITextFieldDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     //outlet needed in order to access the subview
-    
     @IBOutlet var nameField: UITextField!
     @IBOutlet var serialNumberField: UITextField!
     @IBOutlet var valueField: UITextField!
     @IBOutlet var dateLabel: UILabel!
-    
+    @IBOutlet var imageView: UIImageView!
 
     
-    //add a property for an Item instance and override viewWillAppear(_:) to set up the interface
+   
     var item: Item! {
         didSet {
             navigationItem.title = item.name
@@ -50,7 +49,7 @@ class DetailViewController: UIViewController, UITextFieldDelegate {
     }()
     
     
-  
+    //add a property for an Item instance and override viewWillAppear(_:) to set up the interface
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         nameField.text = item.name
@@ -83,6 +82,7 @@ class DetailViewController: UIViewController, UITextFieldDelegate {
         return true
     }
     
+    
     @IBAction func choosePhotoSource(_ sender: UIBarButtonItem) {
         print("Camera button pressed")
         //create an alert controller
@@ -90,19 +90,33 @@ class DetailViewController: UIViewController, UITextFieldDelegate {
                                                 message: nil,
                                                 preferredStyle: .actionSheet)
         
-        //tell alert controller to use the popover presentation style which presents the view in a popover view on Ipad. 
+        //tell alert controller to use the popover presentation style which presents the view in a popover view on Ipad.
         alertController.modalPresentationStyle = .popover
         //specify the bar button item that the popover should point at
         alertController.popoverPresentationController?.barButtonItem = sender
         
-        //Add actions to the alert controller's action sheet
-        let cameraAction = UIAlertAction(title: "Camera", style: .default) { _ in
-            print("Present camera")
+        //only show the camera option if the device has a camera
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            //Add actions to the alert controller's action sheet
+            let cameraAction = UIAlertAction(title: "Camera", style: .default) { _ in
+                print("Present camera")
+                //create an image picker controller instance when the user chooses one of the action sheet options
+                let imagePicker = self.imagePicker(for: .camera)
+                //present the image picker controller modally
+                self.present(imagePicker, animated: true, completion: nil)
+            }
+            alertController.addAction(cameraAction)
         }
-        alertController.addAction(cameraAction)
         
         let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) { _ in
             print("Present photo library")
+            //create an image picker controller instance when the user chooses one of the action sheet options
+            let imagePicker = self.imagePicker(for: .photoLibrary)
+            //camera should be presented full screen and the phot library and saved photos album must be presented in a popover.
+            imagePicker.modalPresentationStyle = .popover
+            imagePicker.popoverPresentationController?.barButtonItem = sender
+            //present the image picker controller instance modally
+            self.present(imagePicker, animated: true, completion: nil)
         }
         alertController.addAction(photoLibraryAction)
         
@@ -113,6 +127,30 @@ class DetailViewController: UIViewController, UITextFieldDelegate {
         present(alertController, animated: true, completion: nil)
     }
 
+    
+    //Adding an image picker controller creation method
+    func imagePicker(for sourceType: UIImagePickerController.SourceType) -> UIImagePickerController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = sourceType
+        //set the instanceof DetailViewController to be the image picker's delegate
+        imagePicker.delegate = self
+        return imagePicker
+    }
+    
+    
+    
+    
+    
+    //Implement the delegate method imagePickerController(_:didFinishPickingMediaWithInfo:). This method is called on the image picker's delegate when a photo has been selected.  This method puts the image into the UIImageView and then call the method to dismiss the image picker.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        //Get picked image from info directory. The image the user selects comes packaged within the info dictionary which contains data relevant to the user's selection and its contents will vary depending on how the image picker is configured.
+        let image = info[.originalImage] as! UIImage
+        //Put that image on the screen in the image view
+        imageView.image = image
+        //In order to take image picker off the screen call the dismiss method
+        dismiss(animated: true, completion: nil)
+    }
+    
     
   
     
